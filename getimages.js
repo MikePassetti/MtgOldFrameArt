bdc9ae057f8057eee6d37a8fb2b94b1402183443
Michael <michaelpassetti8@gmail.com>
2020-10-25 13:12:44 +0200

    added modal and some stuff

--- getimages.js
+++ getimages.js
@@ -3,6 +3,7 @@ var cardimages =[];
 var modal = document.querySelector(".modal"); 
 var closebutton = document.querySelector(".close-button");
 
+// get cardlist data and display card images
 async function getCardList () {
     try {
         let response = await fetch('https://api.scryfall.com/cards/search?order=set&q=e%3Alea&unique=prints');
@@ -13,6 +14,7 @@ async function getCardList () {
     displayCardImage();
 }
 
+// display card images
 function displayCardImage () {
     cardlist.data.forEach(card => {
         cardimages += 
@@ -23,11 +25,68 @@ function displayCardImage () {
     document.querySelector('.cards').innerHTML = cardimages;
 }
 
+// display detailed card image
 function toggleModal () {
     modal.classList.toggle("show-modal");
+    document.querySelector('#modal-image').src = cardlist.data[0].image_uris.png;
 }
 
 closebutton.addEventListener('click', toggleModal);
+
+
+// create card class in order to get to the card properties - name and image uri
+/*class Card {
+    constructor(name, image) {
+        this.cardName = cardlist.data[0].name;
+        this.image = cardlist.data[0].image_uris.png;
+    }
+}*/
+
+// iterate through the cardlist and assign these properties
+
+
+
+
+
+// constructor func for Person objects
+/*function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}*/
+
+/*class ClassName {
+    constructor() { ... }
+    method_1() { ... }
+    method_2() { ... }
+    method_3() { ... }
+    }
+}*/
+
+// create a person object
+// var myFather = new Person("John", "Doe", 50, "blue");
+
+// var card = new Card ("${cardlist.name}", "${cardlist.image}")
+
+// Display age
+/*document.getElementById("demo").innerHTML =
+"My father is " + myFather.age + ".";*/
+
+// document.querySelector("#modal-image").src = card.image;
+
+/*function mapCardList () {
+    cardlist.data.map(card => {
+        card = [card.name, card.image_uris.png]
+        console.log(card);
+    })
+}*/
+
+
+
+
+
+
 
 
 
@@ -54,13 +113,6 @@ closebutton.addEventListener('click', toggleModal);
     .catch((err) => console.log(err))
 }*/
 
-/*function mapCardList () {
-    cardlist.data.map(card => {
-        card = [card.name, card.image_uris.png]
-        console.log(card);
-    })
-}*/
-
 /*function detailedCard () {
     // need to set the src of the modal image = the src of the .cards.img src 
     let indeximage = document.querySelector(".index-image"); 
@@ -72,3 +124,7 @@ closebutton.addEventListener('click', toggleModal);
         }
     })
 }*/
+
+
+
+
